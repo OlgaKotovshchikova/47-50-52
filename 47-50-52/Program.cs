df4731667f4b10b34c5ed4a04cee2f8d9b3f44c0
Olga <olchiknega@mail.ru>
2022-08-28 15:15:40 +0700

    initial commit

---
+++ 47-50-52/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _47_50_52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
