cde2c822314b5e2cdcf6c84b32c6cac38d46da3b
Olga <olchiknega@mail.ru>
2022-08-28 15:55:38 +0700

    task 47 done

--- 47-50-52/Program.cs
+++ 47-50-52/Program.cs
@@ -6,7 +6,80 @@ namespace _47_50_52
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Выберите номер задачи 47, 50, 52: ");
+            int taskNumber = Convert.ToInt32(Console.ReadLine());
+            switch (taskNumber)
+            {
+                case 47:
+                    TaskFourtyseven();
+                    break;
+                case 50:
+                    TaskFifty();
+                    break;
+                case 52:
+                    TaskFiftytwo();
+                    break;
+                default:
+                    Console.WriteLine("Такой задачи не существует");
+                    break;
+            }
         }
+
+        #region Задача 47
+        /*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+        m = 3, n = 4.
+        0,5 7 -2 -0,2
+        1 -3,3 8 -9,9
+        8 7,8 -7,1 9*/
+
+        static void TaskFourtyseven()
+        {
+            Console.Write("Введите количество строк и столбцов через Enter ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            int columns = Convert.ToInt32(Console.ReadLine());
+            double[,] matrix = new double[rows, columns];
+
+            Random rand = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = Math.Round(rand.NextDouble()*10, 2);
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        #endregion
+
+        #region Задача 50
+        /*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+        и возвращает значение этого элемента или же указание, что такого элемента нет.
+        Например, задан массив:
+        1 4 7 2
+        5 9 2 3
+        8 4 2 4
+        1 7 -> такого числа в массиве нет*/
+
+        static void TaskFifty()
+        {
+
+        }
+        #endregion
+
+
+        #region Задача 52
+        /*Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+        Например, задан массив:
+        1 4 7 2
+        5 9 2 3
+        8 4 2 4
+        Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+        static void TaskFiftytwo()
+        {
+
+        }
+        #endregion
     }
 }
